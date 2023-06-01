673543b438d7c18a56455fbc0325bf887a2e55a7
JoeElgendy <yousseftarekfawzy986@gmail.com>
2023-06-02 01:03:45 +0300

    Revert "fixed CoM function"
    This reverts commit 27d8caca901fd85c488fd477c4bac83f00f7bc2a.

--- humanoid_kinematics/src/humanoid_kinematics.cpp
+++ humanoid_kinematics/src/humanoid_kinematics.cpp
@@ -9,10 +9,10 @@ int main(int argc, char **argv)
     nh.getParam("urdf_file", urdf_file);
     kinematics_full humanoid(&nh, urdf_file, "dummy", "Right_Foot_Link", "Left_foot_Link","Right_Lower_Shoulder_link","Left_Lower_Shoulder_Link");
 
-    KDL::ChainFkSolverPos_recursive Right_Leg_fk_solver(humanoid.Right_Leg_Chain);
-    KDL::ChainFkSolverPos_recursive Left_Leg_fk_solver(humanoid.Left_Leg_Chain);
-    KDL::ChainFkSolverPos_recursive Right_Arm_fk_solver(humanoid.Right_Arm_Chain);
-    KDL::ChainFkSolverPos_recursive Left_Arm_fk_solver(humanoid.Left_Arm_Chain);
+    KDL::ChainFkSolverPos_recursive Right_Leg_fk_solver(humanoid.Right_Leg);
+    KDL::ChainFkSolverPos_recursive Left_Leg_fk_solver(humanoid.Left_Leg);
+    KDL::ChainFkSolverPos_recursive Right_Arm_fk_solver(humanoid.Right_Arm);
+    KDL::ChainFkSolverPos_recursive Left_Arm_fk_solver(humanoid.Left_Arm);
     nav_msgs::Path ptest;
     int sampling = 200;
         // boost::shared_ptr<nav_msgs::Path const> sharedPtr;
@@ -25,15 +25,15 @@ int main(int argc, char **argv)
     double home[15] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0 ,0.0};
     humanoid.set_jointpose(home);
     humanoid.set_T_jointpose(home);
-    humanoid.humanoid_will_go_on('d', &Right_Leg_fk_solver, &Left_Leg_fk_solver,&Right_Arm_fk_solver,&Left_Arm_fk_solver,&rate, 70);
-    //humanoid.humanoid_will_go_on('r', &Right_Leg_fk_solver, &Left_Leg_fk_solver,&Right_Arm_fk_solver,&Left_Arm_fk_solver,&rate, 70);
-    // humanoid.humanoid_will_go_on('l', &Right_Leg_fk_solver, &Left_Leg_fk_solver,&Right_Arm_fk_solver,&Left_Arm_fk_solver,&rate, 70);
-    humanoid.T_Right_Thigh_Link = 0.25;
-    humanoid.T_Right_Calf_Link  = 0.25;
-    humanoid.T_Left_Thigh_Link  = 0.25;
-    humanoid.T_Left_Calf_Link   = -0.25;
-    humanoid.humanoid_will_go_on('d', &Right_Leg_fk_solver, &Left_Leg_fk_solver,&Right_Arm_fk_solver,&Left_Arm_fk_solver,&rate, 70);
 
+    humanoid.humanoid_will_go_on('d', &Right_Leg_fk_solver, &Left_Leg_fk_solver,&Right_Arm_fk_solver,&Left_Arm_fk_solver,&rate, 70);
+    humanoid.humanoid_will_go_on('r', &Right_Leg_fk_solver, &Left_Leg_fk_solver,&Right_Arm_fk_solver,&Left_Arm_fk_solver,&rate, 70);
+    humanoid.humanoid_will_go_on('l', &Right_Leg_fk_solver, &Left_Leg_fk_solver,&Right_Arm_fk_solver,&Left_Arm_fk_solver,&rate, 70);
+
+    humanoid.T_Right_Thigh_Link = -0.3;
+    humanoid.T_Right_Calf_Link = 0.55;
+    humanoid.T_Left_Thigh_Link = -0.3;
+    humanoid.T_Left_Calf_Link = 0.55;
     std::cout << "finished 2\n joe \n ";
 
     std::clock_t begin = clock();
